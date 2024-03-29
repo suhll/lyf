c246778f432cad3ca7fb5d884b9c8485deed1918
suhll <149171834+suhll@users.noreply.github.com>
2024-03-29 17:56:07 +0300

    Update poll.js

--- commands/general/poll.js
+++ commands/general/poll.js
@@ -7,7 +7,7 @@ const Polldreaded = async (client, m, text) => {
 let [poll, opt] = text.split("|")
 
 if (text.split("|") < 2)
-                return m.reply(`Wrong format::\nExample:- poll who is the goat|Messi, Lionel, Leo`);
+                return m.reply(`Wrong format::\nExample:- poll who is the gooat|Ronaldo, Cr7, Christiano`);
 
 let options = []
             for (let i of opt.split(',')) {
